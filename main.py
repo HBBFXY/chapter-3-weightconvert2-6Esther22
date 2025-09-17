3cf8767e85353515ff3fd832b50eafa86d4a03dc
6Esther22 <2917038286@qq.com>
2025-09-17 17:54:39 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-# 在这个文件里编写代码
+originalweight=float(input("请输入你的体重(千克):"))
+rate=0.165
+weightfactor=0.5
+for year in range(1,11):
+    earthweight=originalweight+weightfactor*year
+    moonweight=earthweight*rate
+print("10年后你在地球上的体重是:{:.3f}kg,10年后你在月球上的体重是:{:.3f}kg".format(earthweight,moonweight))
