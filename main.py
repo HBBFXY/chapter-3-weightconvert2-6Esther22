ac643ee714947b1fc8bc6b8ce1eeefe83c31c654
6Esther22 <2917038286@qq.com>
2025-09-19 11:13:37 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-originalweight=50
-rate=0.165
-weightfactor=0.5
+#WeightConvert2.py
+initial_earth_weight=50
+lunar_radio=0.165
+annual_growth=0.5
+print("年份\t地球体重(kg)\t月球体重(kg)")
 for year in range(1,11):
-    earthweight=originalweight+weightfactor*year
-    moonweight=earthweight*rate
-print("10年后你在地球上的体重是:{:.3f}kg,10年后你在月球上的体重是:{:.3f}kg".format(earthweight,moonweight))
+    earth_weight=initial_earth_weight+year*annual_growth
+    lunar_weight=earth_weight*lunar_radio
+    print(f"{year}\t{earth_weight:.2f}\t\t{lunar_weight:.2f}")
